6f82b148400aa1c65406af908bba39bdd946d4c5
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-12-08 10:59:46 -0600

    [Client]: all dependencies are installed and files are created. NavBar is created.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { store } from './app/store.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <ToastContainer/>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
